e83f82d87048dcad446d46bbe0e0ef105805729d
masa <hojonohoro@gmail.com>
2026-08-07 22:50:05 +0900

    Add files via upload

---
+++ User.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+class User
+{
+    public static void main(String[]args) throws IOException
+    {
+        System.out.println("Kana:こんにちは！");
+        System.out.println("Kana:名前を教えてね。");
+
+        BufferedReader br =
+        new BufferedReader (new InputStreamReader(System.in,"MS932"));
+        String str = br.readLine();
+        System.out.println("Kana:"+str+"、よろしくね！");
+        System.out.println("Kana:今日も頑張ろう！");
+    }
+
+}
