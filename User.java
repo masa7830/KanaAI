8045c85f0b6102ae49a41e7c22444572ba8d9f0b
masa7830 <hojonohoro@gmail.com>
2026-08-08 22:46:53 +0900

    Kanaに会話ループとキーワード反応を追加

--- User.java
+++ User.java
@@ -8,23 +8,26 @@ class User
 {
     public static void main(String[]args) throws IOException
     {
+        //起動のあいさつ
         System.out.println("Kana:おかえりなさい！");
         System.out.println("Kana:名前を教えてね。");
 
         BufferedReader br =
         new BufferedReader (new InputStreamReader(System.in,"MS932"));
       
-    
+        //ユーザー名を追加
         String str = br.readLine();
 
         System.out.println("Kana:" + str + "、よろしくね！");
+        
+        //今日の気分を聞く
         System.out.println("Kana:今日の気分はどう？");
         System.out.println("Kana:1. 元気！");
         System.out.println("Kana:2. まあまあかな");
         System.out.println("Kana:3. 疲れた");
-        //キーボードから入力された文字列を整数に変換
+        //キーボードから入力された文字列を整数に変換してmoodに保存
         int mood = Integer.parseInt(br.readLine());
-
+        //1から3以外が入力されたら正しい数字になるまで聞き返す
         while (mood != 1 && mood != 2 && mood != 3) {
 
             System.out.println("Kana:ごめん、よく分からなかった");
@@ -33,9 +36,11 @@ class User
             System.out.println("Kana:1. 元気！");
             System.out.println("Kana:2. まあまあかな");
             System.out.println("Kana:3. 疲れた");
+
+            //moodの値を新しい入力で更新
             mood = Integer.parseInt(br.readLine());
         }
-
+        //気分に応じてKanaの返事を変える
         if (mood == 1) {
             System.out.println("Kana:元気そうでよかった！");
         }
@@ -49,6 +54,7 @@ class User
             System.out.println("Kana:うーん、よく分からなかった");
         }
      
+        //会話を続けるか確認
         System.out.println("Kana:まだ話す？");
         System.out.println("Kana:1.もう少し話をする");
         System.out.println("Kana:2.今日はもう帰る");
@@ -56,15 +62,18 @@ class User
         int choice = Integer.parseInt(br.readLine());
         
 
+        //1か2以外なら聞き返す
         while (choice != 1 && choice != 2) {
             System.out.println("Kana:ごめん、よく分からなかった");
             System.out.println("Kana:もう一度選んでね！");
             System.out.println("Kana:1.もう少し話をする");
             System.out.println("Kana:2.今日はもう帰る");
 
+        //choiceを新しい入力で更新
         choice = Integer.parseInt(br.readLine());
         }
        
+        //帰ろうとしたら一度だけ引き留める
         if (choice ==1){
             System.out.println("Kana:たくさん話そう");
         }
@@ -73,8 +82,10 @@ class User
             System.out.println("Kana:1.やっぱりもう少し話す");
             System.out.println("Kana:2.うん、今日は帰る");
 
+            //すでにchoice宣言済みなのでintはつけない
             choice = Integer.parseInt(br.readLine());
 
+            //1か2以外ならもう一度確認
             while (choice != 1 && choice != 2) {
                 System.out.println("Kana:ごめん、よく分からなかった");
                 System.out.println("Kana:もう一度選んでね！");
@@ -84,6 +95,8 @@ class User
                 choice = Integer.parseInt(br.readLine());
             }
         }
+
+            //choiceが1の間会話を続ける
             if (choice ==1){
             
                  while (choice == 1) {
@@ -91,19 +104,34 @@ class User
 
                     System.out.println("Kana:何を話す？");
 
+                    //会話内容を文字列として受け取る
                     String talk = br.readLine();
 
-                    System.out.println("Kana:" + talk + "なんだね");
+                    //疲れた勉強に反応、それ以外なら相槌
 
+                    if (talk.contains("疲れた")) {
+                        System.out.println("Kana:お疲れさま。今日はゆっくりしよ？");
+                    }
+                    
+                    else if (talk.contains("勉強")) {
+                        System.out.println("Kana:勉強したんだ！お疲れさま！");
+                    }
+
+                    else {
+                        System.out.println("Kana:" + talk + "なんだね");
+                    }
+
+                    //会話を続けるか確認
                     System.out.println("Kana:もっと話そう！");
                     System.out.println("Kana:1.もう少し話す");
                     System.out.println("Kana:2.今日は帰る");
 
+                    //1ならもう一周、
                     choice = Integer.parseInt(br.readLine());
             }
      }
     
-
+            //終了時のあいさつ
             System.out.println("行ってらっしゃい！また来てね！！ばいばーい");
         
             
